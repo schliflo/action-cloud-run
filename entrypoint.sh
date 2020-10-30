060505fcef6be434f2ec3b8bc933ad6bd301f761
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2020-10-30 13:59:51 +0100

    move commands to main file

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,7 +41,13 @@ echo "SERVICE_NAME = ${SERVICE_NAME}"
 echo "\n\n-----------------------------------------------------------------------------\n\n"
 
 echo "\nCreate GitHub Deployment..."
-sh deployment-create.sh
+curl \
+  -v \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$REPO/deployments \
+  -d "{'ref': '$BRANCH'}"
 
 # service key
 
