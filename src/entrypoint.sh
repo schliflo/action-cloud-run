48d535bef53ee5d207006edaf4ce8a5806356309
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2022-03-03 17:42:40 +0100

    push all image tags

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -112,7 +112,7 @@ if [ "$INPUT_HOOK_PUSH_BEFORE" ]; then
 fi
 
 echo -e "\nPush image..."
-docker push "$GCR_IMAGE_NAME"
+docker push -a "$GCR_IMAGE_NAME"
 
 if [ "$INPUT_HOOK_PUSH_AFTER" ]; then
   sh $INPUT_HOOK_PUSH_AFTER
